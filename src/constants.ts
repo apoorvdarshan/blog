37448c908c2165102e3e75fb8f945a5b1af5b7b1
Apoorv Darshan <ad13dtu@gmail.com>
2026-03-06 00:02:29 +0530

    Update social links to @apoorvdarshan and timezone to Asia/Kolkata

--- src/constants.ts
+++ src/constants.ts
@@ -20,19 +20,19 @@ interface Social {
 export const SOCIALS: Social[] = [
   {
     name: "GitHub",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/apoorvdarshan",
     linkTitle: `${SITE.title} on GitHub`,
     icon: IconGitHub,
   },
   {
     name: "X",
-    href: "https://x.com/username",
+    href: "https://x.com/apoorvdarshan",
     linkTitle: `${SITE.title} on X`,
     icon: IconBrandX,
   },
   {
     name: "LinkedIn",
-    href: "https://www.linkedin.com/in/username/",
+    href: "https://www.linkedin.com/in/apoorvdarshan/",
     linkTitle: `${SITE.title} on LinkedIn`,
     icon: IconLinkedin,
   },
